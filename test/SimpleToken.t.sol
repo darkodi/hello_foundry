ab22ecd8d6913641423767d9adfa202d01a28b73
darkodi <darko.deuric@ethernal.tech>
2023-12-03 11:32:12 +0100

    invariant testing

---
+++ test/SimpleToken.t.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+contract SimpleToken {
+    mapping(address => uint256) public balances;
+
+    constructor(uint256 initialSupply) {
+        balances[msg.sender] = initialSupply;
+    }
+
+    function transfer(address to, uint256 amount) public {
+        require(balances[msg.sender] >= amount, "Not enough tokens");
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+
+    function getBalance(address user) public view returns (uint256) {
+        return balances[user];
+    }
+}
+
+contract SimpleTokenTest is Test {
+    SimpleToken token;
+    uint256 constant INITIAL_SUPPLY = 1000;
+    address[] testUsers;
+
+    function setUp() public {
+        token = new SimpleToken(INITIAL_SUPPLY);
+
+        // Create test user accounts
+        testUsers.push(address(0x123)); // hypothetical addresses
+        testUsers.push(address(0x456));
+        testUsers.push(address(0x789));
+        
+        // Distribute some initial tokens to test users
+        for (uint i = 0; i < testUsers.length; i++) {
+            token.transfer(testUsers[i], 100); // Distribute 100 tokens to each test user
+        }
+    }
+
+    function getTestUsers() public view returns (address[] memory) {
+        return testUsers;
+    }
+
+    // Standard tests...
+
+    // Invariant testing
+    function invariant_TotalSupply() public {
+        uint256 totalSupply = token.getBalance(address(this)); // Include the contract's balance in the total
+
+        // Sum up all balances of test users
+        for (uint i = 0; i < testUsers.length; i++) {
+            totalSupply += token.getBalance(testUsers[i]);
+        }
+
+        // Assert that the total supply hasn't changed
+        assertEq(totalSupply, INITIAL_SUPPLY);
+    }
+    /** 
+        The invariant checks passes confirming that the total supply of tokens remains constant at 1000, as expected.
+        This process demonstrates that despite the random and varied transfers between users, 
+        the total supply of tokens in the system remains unchanged, validating the core logic of the SimpleToken contract.
+     */
+}
