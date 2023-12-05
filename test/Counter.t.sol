0f9b3a7370aa396f2a99e44828647dbb7a8ce0ca
darkodi <darko.deuric@ethernal.tech>
2023-12-05 14:02:42 +0100

    Fail test scenarios

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,15 +1,24 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console2} from "forge-std/Test.sol";
+import {Test, console2, stdError} from "forge-std/Test.sol";
 import {Counter} from "../src/Counter.sol";
 
 contract CounterTest is Test {
     Counter public counter;
 
-    function setUp() public {
+    function setUp() public { // executed before each test
         counter = new Counter();
         counter.setNumber(0);
+    }
+
+    function testFail_Decrement() public { // Foundry expects this test to fail (because of Fail word in the name), and it fails!
+        counter.decrement();
+    }
+
+    function testDecrementUndeflow() public {
+        vm.expectRevert(stdError.arithmeticError); // saying to foundry that we expect underflow error
+        counter.decrement(); 
     }
 
     function test_Increment() public {
