f980512b01205245b80058f4de27b65bede5af42
darkodi <darko.deuric@ethernal.tech>
2023-12-06 15:46:41 +0100

    time contract, fail case before start time

---
+++ src/Time.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.18;
+import "forge-std/console.sol";
+
+contract Auction {
+    uint256 public startAt = block.timestamp + 1 days;
+    uint256 public endAt = block.timestamp + 2 days;
+
+    function bid() external {
+        console.logUint(block.timestamp);
+        console.logUint(startAt);
+        console.logUint(endAt);
+        require(
+            block.timestamp >= startAt && block.timestamp < endAt, "cannot bid"
+        );
+    }
+
+    function end() external {
+        require(block.timestamp >= endAt, "cannot end");
+    }
+}
---
+++ test/Time.t.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.18;
+
+import "forge-std/Test.sol";
+import {Auction} from "../src/Time.sol";
+
+contract TimeTest is Test {
+    Auction public auction;
+    uint256 private startAt;
+
+    // vm.warp - set block.timestamp to future timestamp
+    // vm.roll - set block.number
+    // skip - increment current timestamp
+    // rewind - decrement current timestamp
+
+    function setUp() public {
+        auction = new Auction();
+        startAt = block.timestamp;
+    }
+
+    function testBidFailsBeforeStartTime() public {
+        vm.expectRevert(bytes("cannot bid")); 
+        auction.bid(); // will revert because block.timestamp = 1
+    }
+}
