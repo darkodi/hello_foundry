00d846ead7120997e0062e6c4b906ca8dc821bc2
darkodi <darko.deuric@ethernal.tech>
2023-12-06 16:52:45 +0100

    deal, prank, hoax

--- test/Wallet.t.sol
+++ test/Wallet.t.sol
@@ -27,4 +27,26 @@ contract WalletTest is Test {
         (bool ok,) = address(wallet).call{value: amount}("");
         require(ok, "send ETH failed");
     }
+     function testSendEth() public {
+        uint256 bal = address(wallet).balance;
+
+        // set balance to some address
+        deal(address(1), 100);
+        assertEq(address(1).balance, 100);
+
+        deal(address(1), 10);
+        assertEq(address(1).balance, 10);
+
+        
+        deal(address(1), 123); // balance[address 1] = 123
+        vm.prank(address(1)); // msg.sender is address 1
+        _send(123); // send 123 to wallet
+
+        // the same thing could be achieved with hoax (deal + prank) 
+        hoax(address(1), 456);
+        _send(456); // send 456 to wallet
+
+        assertEq(address(wallet).balance, bal + 123 + 456); // total balance
+    }
+
 }
